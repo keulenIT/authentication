7454bb06349a9473c41f6a74ca087d011b20d6d1
Mathias Keulen <mathias.keulen@external.engie.com>
2024-12-15 15:41:01 +0100

    feat(authentication): add rule

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyewr-L4_2CYUsNXExcGwn9eQ4JblRwAs",
+  authDomain: "authentication-project-29022.firebaseapp.com",
+  projectId: "authentication-project-29022",
+  storageBucket: "authentication-project-29022.firebasestorage.app",
+  messagingSenderId: "60678278332",
+  appId: "1:60678278332:web:0d71d1768c3c02b71a54b9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
