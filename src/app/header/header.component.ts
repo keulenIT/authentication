7d813ba33d5765bd4078f8b24829a714c5b21525
Mathias Keulen <mathias.keulen@external.engie.com>
2024-12-15 15:14:47 +0100

    feat(authentication): add firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,7 +4,7 @@ import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
   constructor(private dataStorageService: DataStorageService) {}
